cd5e21e656fb26644e1be0e5a0432cb439713d59
theCodography <hungfungq@gmail.com>
2026-04-01 22:27:13 +0700

    add env

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // TODO: Thay YOUR_RENDER_URL bằng URL thực tế sau khi deploy lên Render
   // Ví dụ: 'https://japan-quizlet-api.onrender.com/api'
-  apiUrl: 'https://YOUR_RENDER_URL/api'
+  apiUrl: 'https://japanese-quizlet.onrender.com/api',
 };
